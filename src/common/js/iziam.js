842f0151c2981b16df0f0401782b432033d8121e
Pierre Wieser <pwieser@trychlos.org>
2023-12-24 16:56:02 +0100

    Use constant defined by izIAM instead of local string

--- src/common/js/iziam.js
+++ src/common/js/iziam.js
@@ -23,8 +23,8 @@ if( Meteor.isClient ){
         izIAM.requestCredential( options, credentialRequestCompleteCallback );
     };
 
-    Accounts.registerClientLoginFunction( 'iziam', loginWithIzIAM );
-    Meteor.loginWithIzIAM = ( ...args ) => Accounts.applyLoginFunction( 'iziam', args );
+    Accounts.registerClientLoginFunction( izIAM.C.Service, loginWithIzIAM );
+    Meteor.loginWithIzIAM = ( ...args ) => Accounts.applyLoginFunction( izIAM.C.Service, args );
 
 } else {
 
@@ -32,7 +32,7 @@ if( Meteor.isClient ){
         // not sure whether the github api can be used from the browser,
         // thus not sure if we should be sending access tokens; but we do it
         // for all other oauth2 providers, and it may come in handy.
-        forLoggedInUser: [ 'services.iziam' ],
-        forOtherUsers: [ 'services.iziam.username' ]
+        forLoggedInUser: [ 'services.'+izIAM.C.Service ],
+        forOtherUsers: [ 'services.'+izIAM.C.Service+'.username' ]
     });
 }
